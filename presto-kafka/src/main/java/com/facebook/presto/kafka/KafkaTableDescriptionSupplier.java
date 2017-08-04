479dfe1a245e741bf5e0fc6806a84990b000749b
zenmyo_teruyoshi <zenmyo_teruyoshi@cyberagent.co.jp>
2017-08-04 16:46:22 +0900

    use AdminClient to get list of topics

--- presto-kafka/src/main/java/com/facebook/presto/kafka/KafkaTableDescriptionSupplier.java
+++ presto-kafka/src/main/java/com/facebook/presto/kafka/KafkaTableDescriptionSupplier.java
@@ -19,24 +19,27 @@ import com.google.common.base.Splitter;
 import com.google.common.base.Throwables;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
 import io.airlift.json.JsonCodec;
 import io.airlift.log.Logger;
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.TopicListing;
 
 import javax.inject.Inject;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 import static com.google.common.base.MoreObjects.firstNonNull;
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Strings.isNullOrEmpty;
 import static java.nio.file.Files.readAllBytes;
-import static java.util.Arrays.asList;
 import static java.util.Objects.requireNonNull;
 
 public class KafkaTableDescriptionSupplier
@@ -47,7 +50,7 @@ public class KafkaTableDescriptionSupplier
     private final JsonCodec<KafkaTopicDescription> topicDescriptionCodec;
     private final File tableDescriptionDir;
     private final String defaultSchema;
-    private final Set<String> tableNames;
+    private final Properties properties;
 
     @Inject
     KafkaTableDescriptionSupplier(KafkaConnectorConfig kafkaConnectorConfig,
@@ -58,7 +61,8 @@ public class KafkaTableDescriptionSupplier
         requireNonNull(kafkaConnectorConfig, "kafkaConfig is null");
         this.tableDescriptionDir = kafkaConnectorConfig.getTableDescriptionDir();
         this.defaultSchema = kafkaConnectorConfig.getDefaultSchema();
-        this.tableNames = ImmutableSet.copyOf(kafkaConnectorConfig.getTableNames());
+        this.properties = new Properties();
+        this.properties.put("bootstrap.servers", kafkaConnectorConfig.getNodes().stream().map(h -> String.format("%s:%s", h.getHostText(), h.getPort())).collect(Collectors.joining(",")));
     }
 
     @Override
@@ -68,54 +72,42 @@ public class KafkaTableDescriptionSupplier
 
         log.debug("Loading kafka table definitions from %s", tableDescriptionDir.getAbsolutePath());
 
-        try {
-            for (File file : listFiles(tableDescriptionDir)) {
+        try (AdminClient adminClient = AdminClient.create(properties)) {
+            Collection<TopicListing> topics = adminClient.listTopics().listings().get();
+            for (TopicListing topic : topics) {
+                File file = new File(tableDescriptionDir, topic.name() + ".json");
                 if (file.isFile() && file.getName().endsWith(".json")) {
                     KafkaTopicDescription table = topicDescriptionCodec.fromJson(readAllBytes(file.toPath()));
                     String schemaName = firstNonNull(table.getSchemaName(), defaultSchema);
-                    log.debug("Kafka table %s.%s: %s", schemaName, table.getTableName(), table);
+                    log.debug("Found Table definition for %s: %s", table.getTableName(), topic.name());
                     builder.put(new SchemaTableName(schemaName, table.getTableName()), table);
                 }
-            }
-
-            Map<SchemaTableName, KafkaTopicDescription> tableDefinitions = builder.build();
-
-            log.debug("Loaded Table definitions: %s", tableDefinitions.keySet());
-
-            builder = ImmutableMap.builder();
-            for (String definedTable : tableNames) {
-                SchemaTableName tableName;
-                try {
-                    tableName = parseTableName(definedTable);
-                }
-                catch (IllegalArgumentException iae) {
-                    tableName = new SchemaTableName(defaultSchema, definedTable);
-                }
-
-                if (tableDefinitions.containsKey(tableName)) {
-                    KafkaTopicDescription kafkaTable = tableDefinitions.get(tableName);
-                    log.debug("Found Table definition for %s: %s", tableName, kafkaTable);
-                    builder.put(tableName, kafkaTable);
-                }
                 else {
-                    // A dummy table definition only supports the internal columns.
+                    SchemaTableName tableName = new SchemaTableName(defaultSchema, topic.name());
                     log.debug("Created dummy Table definition for %s", tableName);
-                    builder.put(tableName, new KafkaTopicDescription(tableName.getTableName(),
+                    builder.put(tableName,
+                            new KafkaTopicDescription(tableName.getTableName(),
                             tableName.getSchemaName(),
-                            definedTable,
+                            topic.name(),
                             new KafkaTopicFieldGroup(DummyRowDecoder.NAME, ImmutableList.of()),
                             new KafkaTopicFieldGroup(DummyRowDecoder.NAME, ImmutableList.of())));
                 }
             }
-
-            return builder.build();
         }
         catch (IOException e) {
             log.warn(e, "Error: ");
             throw Throwables.propagate(e);
         }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        return builder.build();
     }
 
+    /*
     private static List<File> listFiles(File dir)
     {
         if ((dir != null) && dir.isDirectory()) {
@@ -127,6 +119,7 @@ public class KafkaTableDescriptionSupplier
         }
         return ImmutableList.of();
     }
+    */
 
     private static SchemaTableName parseTableName(String schemaTableName)
     {
